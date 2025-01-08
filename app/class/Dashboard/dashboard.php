51b1bb8dd47ced6f80e100e51dbdccc872ce2d92
rayan4-dot <rayanpro2023@gmail.com>
2025-01-08 01:22:17 +0100

    stats dynamic

---
+++ app/class/Dashboard/dashboard.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Class\Dashboard;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use App\Class\Crud\Crud;
+
+class Dashboard extends Crud {
+
+    public function getTotalUsers() {
+        $result = $this->selectRecords('users', 'COUNT(*) AS total');
+        // return $result ? [['total' => 0]] : null;
+    }       
+
+    public function getTotalArticles() {
+        $result = $this->selectRecords('articles', 'COUNT(*) AS total');
+        // return $result ? [['total' => 0]] : null;
+    }
+    public function getTotalCategories() {
+        $result = $this->selectRecords('categories', 'COUNT(*) AS total');
+        return $result ? $result[0]['total'] : 0;
+    }
+}
+
+?>
