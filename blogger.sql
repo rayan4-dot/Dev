24f11c27053bc9accec1872c005a73e3ef1fab6b
rayan4-dot <rayanpro2023@gmail.com>
2025-01-09 20:52:20 +0100

    autoincrement author_id

--- blogger.sql
+++ blogger.sql
@@ -1,50 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 09, 2025 at 08:51 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blogger`
+--
 
 -- --------------------------------------------------------
 
 --
-
-
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
+-- Table structure for table `articles`
 --
--- Table structure for table `categories`
---
-
-
-
--- --------------------------------------------------------
-
-
-
-CREATE TABLE `categories` (
-  `id` bigint(20) NOT NULL,
-  `name` text NOT NULL
-) 
-
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `bio` text DEFAULT NULL,
-  `profile_picture_url` varchar(255) DEFAULT NULL,
-  `role` enum('user','author','admin') NOT NULL DEFAULT 'user'
-)--
-
-CREATE TABLE `tags` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL
-) 
-
-
-CREATE TABLE `article_tags` (
-  `article_id` bigint(20) UNSIGNED NOT NULL,
-  `tag_id` bigint(20) NOT NULL
-) 
 
 CREATE TABLE `articles` (
   `id` bigint(20) UNSIGNED NOT NULL,
@@ -61,8 +42,88 @@ CREATE TABLE `articles` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `views` int(11) DEFAULT 0
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_tags`
+--
+
+CREATE TABLE `article_tags` (
+  `article_id` bigint(20) UNSIGNED NOT NULL,
+  `tag_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(19, 'sport'),
+(24, 'Lifestyle'),
+(25, 'Health'),
+(26, 'Travel'),
+(28, 'Technology'),
+(29, 'Lifestyle');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`) VALUES
+(6, 'rayan'),
+(7, 'yo'),
+(9, 'rayan'),
+(10, 'yo'),
+(11, 'rayané'),
+(12, 'kid'),
+(13, 'kudo'),
+(14, 'phrase'),
+(16, 'jhlk'),
+(17, '12'),
+(18, '6'),
+(19, '13'),
+(20, '14'),
+(21, '19'),
+(22, 'Innovation'),
+(23, 'Productivity'),
+(24, 'Wellness'),
+(25, 'Adventure'),
+(26, 'Tutorial'),
+(27, 'Innovation'),
+(28, 'Productivity'),
+(29, 'Wellness'),
+(30, 'Adventure'),
+(31, 'Tutorial');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL,
@@ -72,21 +133,16 @@ CREATE TABLE `users` (
   `bio` text DEFAULT NULL,
   `profile_picture_url` varchar(255) DEFAULT NULL,
   `role` enum('user','author','admin') NOT NULL DEFAULT 'user'
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tags`
-
-INSERT INTO `tags` (`id`, `name`) VALUES
-
-
--- --------------------------------------------------------
-
+-- Dumping data for table `users`
 --
--- Table structure for table `users`
--
 
- ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `bio`, `profile_picture_url`, `role`) VALUES
+(4, 'kudo', 'rayanpro2023@gmail.com', '$2y$10$HI0tMKGWEXg3L0NJY22FkO8XL1Q5o..WtwMEWNeTJ5B.I4BRoJljK', NULL, NULL, 'admin'),
+(6, 'imane', 'bobo@gmail.com', '$2y$10$c/lU3m3g5cVwLvXyXsXPYuEG57y0szQCBde2xI70Atf.X7HJXe6jC', NULL, NULL, 'author'),
+(10, 'aymane', 'wldinzegane@gmail.com', '$2y$10$kbyAs5jLqfB12CQRlqowCesSWdHEWtAffdqTdNaDzxwJlVS9TyVkG', NULL, NULL, 'user');
 
 --
 -- Indexes for dumped tables
@@ -126,32 +182,32 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped table
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
